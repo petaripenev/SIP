8dc0dba748fd0ede51501e777d2bbf32bf788a3a
Petar Penev <peteripenev@gmail.com>
2022-09-14 16:36:55 -0700

    Add check for even number of D1 and D2. Add more filters

--- scripts/randomize_SIP.py
+++ scripts/randomize_SIP.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from tokenize import group
 import numpy as np
 from random import shuffle, seed
 import csv, re, sys
@@ -9,9 +10,15 @@ def check_replicates(groupedEntries:list):
     For example HB4_D2... and HA4_D2...'''
     removed_isotope_incubationLength = [x[:6] for x in groupedEntries]
     removed_replicate_identifier = [x[0]+x[2:] for x in removed_isotope_incubationLength]
-    if len(set(removed_replicate_identifier)) < len(groupedEntries):
-        return True
-    return False
+    return len(set(removed_replicate_identifier)) < len(groupedEntries)
+
+def check_weights(groupedEntries:list):
+    '''Checks a list for even number of D1-2 samples.'''
+    d1_or_d2 = list()
+    for x in groupedEntries:
+        if '_D1_' in x or '_D2_' in x:
+            d1_or_d2.append(x)
+    return len(d1_or_d2) % 2 != 0
 
 def find_and_remove_failed_shuffles(list_of_groupedEntries:list):
     '''Finds groups of entries where the repeated replicate rule fails,
@@ -19,7 +26,7 @@ def find_and_remove_failed_shuffles(list_of_groupedEntries:list):
     separate list in addition to the succeeded list'''
     removal_idx, failed_shuffles = list(), list()
     for i, x in enumerate(list_of_groupedEntries):
-        if check_replicates(x):
+        if check_replicates(x) or check_weights(x):
             removal_idx.append(i)
             failed_shuffles.append(x)
 
@@ -55,28 +62,31 @@ sip_entries_withDB18, sip_entries_withDB16 = dict(), dict()
 n = 6
 with open('./data/all_samples_afterW7.csv') as csvFile:
     reader = csv.reader(csvFile)
+    #seed(int(sys.argv[1]))
+    seed(107)
     for row in reader:
-        if '6_D' in row[1]:
+        #Site to exclude
+        if 'H' == row[1][0]:
             continue
-        if '_D3_' in row[1]:
+        #Weeks to exclude
+        if '6_D' in row[1] or '1_D' in row[1] or '4_D' in row[1]:
             continue
-        
+        #Depths to exclude
+        if '_D4_' in row[1] or '_D2_' in row[1]:
+            continue
         #If we are doing only D1 and D2
-        if '_D4_' in row[1]:
-            continue
-        if '_D5_' in row[1]:
-            continue
+        # if '_D4_' in row[1]:
+        #     continue
+        # if '_D5_' in row[1]:
+        #     continue
         #Depending on initial SIP results:
-        # if '-30' in row[1]:
-        #    seed(26)
-        #    continue
+        if '-30' in row[1]:
+           #seed(26)
+           continue
         # if re.match(r'.{3}_D[4|5]_.{3}-7', row[1]):
         #     seed(24)
         #     continue
-        #seed(int(sys.argv[1]))
         
-        #For D1 D2 only
-        seed(32)
         if '18O-' in row[1]:
             sip_entries_withDB18[row[1]] = row[0].split('/')[4]
             sip_entries18.append(row[1])
@@ -89,9 +99,11 @@ shuffled_16O = recursively_shuffle_until_noFails(sip_entries16)
 shuffled_18O = recursively_shuffle_until_noFails(sip_entries18)
 
 for x in shuffled_16O:
+    print(','.join(x))
     print(','.join([sip_entries_withDB16[y] for y in x]))
 print(len(sip_entries16), len(shuffled_16O))
 
 for x in shuffled_18O:
+    print(','.join(x))
     print(','.join([sip_entries_withDB18[y] for y in x]))
 print(len(sip_entries18), len(shuffled_18O))
