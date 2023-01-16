42645b102b607b65eeb416a20f77acd5e765d9f4
Petar Penev <peteripenev@gmail.com>
2023-01-15 21:47:23 -0800

    Start DNAextractionSIPSample

--- scripts/waterYearSamplesInfrastructure.py
+++ scripts/waterYearSamplesInfrastructure.py
@@ -2,8 +2,9 @@
 from csv import DictReader
 from os.path import isfile
 from datetime import date
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields
 from typing import Tuple
+from warnings import warn
 
 @dataclass
 class WaterYearSample:
@@ -19,56 +20,68 @@ class WaterYearSample:
 
     def __post_init__(self):
         self.date = date.fromisoformat(self.date)
-        self.sample_id = self.__repr__()
-        self.oneword_id = self.sample_id.replace('-','').replace('_','')
+        self.core_id = f"{self.sampling_site}{self.replicate}{self.sampling_week}_{self.depth_str}"
+        self.oneword_id = self.core_id.replace('-','').replace('_','')
         if not isfile(self.metadata_file_path):
             raise FileNotFoundError(f"Metadata file {self.metadata_file_path} not found!")
         if self.core_GWC is None:
-            self.core_GWC = self._get_GSC_property(self.metadata_file_path, 'GWC %')
+            self.core_GWC = self._get_experiment_property(self.metadata_file_path, self.core_id, 'GSC','GWC %')
         if self.core_pH is None:
-            self.core_pH = self._get_GSC_property(self.metadata_file_path, 'pH')
+            self.core_pH = self._get_experiment_property(self.metadata_file_path, self.core_id, 'GSC', 'pH')
 
     def __repr__(self):
         return f"{self.sampling_site}{self.replicate}{self.sampling_week}_{self.depth_str}"
 
-    def _get_GSC_property(self, filePath, property):
-        '''Get a property of a sample from the metadata file'''
+    def _get_experiment_property(self, filePath, core_id, experiment, property):
+        '''Get a property of a sample from the metadata file, 
+        given the experiment name and the property name.
+        Assumes the property is a float.'''
         with open(filePath) as csvfile:
             reader = DictReader(csvfile)
             for row in reader:
-                if row['sample_id'] == f"{self.sample_id}_GSC":
-                    if property not in row.keys():
-                        raise KeyError(f"Property {property} not found in metadata file at row {row['sample_id']}!")
-                    if row[property] == '':
-                        raise ValueError(f"Property {property} is empty in metadata file at row {row['sample_id']}!")
-                    return float(row[property])
+                if row['sample_id'] != f"{core_id}_{experiment}":
+                    continue
+                if property not in row.keys():
+                    raise KeyError(f"Property {property} not found in metadata file at row {row['sample_id']}!")
+                if row[property] == '':
+                    warn(f"Property {property} is empty in metadata file at row {row['sample_id']}!", RuntimeWarning)
+                    return None
+                return float(row[property])
+        raise KeyError(f"Sample {core_id}_{experiment} not found in metadata file!")
 
 @dataclass
-class UnfracSIPSample(WaterYearSample):
+class DNAextractionSIPSample(WaterYearSample):
     isotope: str
     incubation_length: int
-    grams_soil_extracted: float
-    concentration_DNA_extracted: float
-    total_ul_DNA_extracted: float
-    h20_added: float
+    grams_soil_extracted: float = field(default=None, init=False)
+    concentration_DNA_extracted: float = field(default=None, init=False)
+    total_ul_DNA_extracted: float = field(default=None, init=False)
+    h20_added: float = field(default=None, init=False)
+    ATTRIBUTES_TO_PROPERTIES = {
+        'grams_soil_extracted': 'Grams soil for DNA extraction',
+        'concentration_DNA_extracted': 'DNA concentration (ng/ul)',
+        'total_ul_DNA_extracted': 'Volume DNA extraction (ul)',
+        'h20_added': 'H2O amount (ml)'
+    }
 
     def __post_init__(self):
-        self.sample_id = self.__repr__()
-        self.oneword_id = self.sample_id.replace('-','').replace('_','')
+        super().__post_init__()
+        self.sample_id = f"{self.sampling_site}{self.replicate}{self.sampling_week}_{self.depth_str}"
+        self.experiment = f"{self.isotope}-{self.incubation_length}"
+        none_attributes = [field.name for field in fields(self) if getattr(self, field.name) is None]
+        for attribute in none_attributes:
+            if attribute not in self.ATTRIBUTES_TO_PROPERTIES.keys():
+                continue
+            property = self.ATTRIBUTES_TO_PROPERTIES[attribute]
+            setattr(self, attribute, self._get_experiment_property(self.metadata_file_path, 
+                self.core_id, self.experiment, property))
 
     def __repr__(self):
-        return f"{self.sampling_site}{self.replicate}{self.sampling_week}_{self.depth[0]}-{self.depth[1]}_{self.isotope}O-{self.incubation_length}"
-    
+        return f"{self.sampling_site}{self.replicate}{self.sampling_week}_{self.depth_str}_{self.isotope}-{self.incubation_length}"
 
 @dataclass
-class SIPSample(UnfracSIPSample):
-    isotope: str
-    incubation_length: int
-    grams_soil_extracted: float
-    concentration_DNA_extracted: float
-    total_ul_DNA_extracted: float
+class FractionatedSIPSample(DNAextractionSIPSample):
     ul_DNA_SIP_loaded: float
-    h20_added: float
     plate: str
     tube: str
     dna_yield: float
--- tests/test_WaterYearInfra.py
+++ tests/test_WaterYearInfra.py
@@ -1,5 +1,5 @@
 import unittest
-from scripts.waterYearSamplesInfrastructure import WaterYearSample
+from scripts.waterYearSamplesInfrastructure import WaterYearSample, DNAextractionSIPSample
 
 class TestWaterYearSample(unittest.TestCase):
     def testPost(self):
@@ -11,7 +11,7 @@ class TestWaterYearSample(unittest.TestCase):
         self.assertEqual(s1.depth_str, 'D1')
         self.assertEqual(str(s1.date), '2020-01-01')
         self.assertEqual(s1.metadata_file_path, './data/metadata/all_samples.csv')
-        self.assertEqual(s1.sample_id, 'AA1_D1')
+        self.assertEqual(s1.core_id, 'AA1_D1')
         self.assertEqual(s1.oneword_id, 'AA1D1')
         self.assertEqual(s1.core_GWC, 24.7396979)
         self.assertEqual(s1.core_pH, 4.63)
@@ -40,13 +40,18 @@ class TestWaterYearSample(unittest.TestCase):
 
     def testGet_experiment_property(self):
         s1 = WaterYearSample('A', 'A', '1', (0, 10), 'D1', '2020-01-01', './data/metadata/all_samples.csv')
-        self.assertEqual(s1._get_experiment_property('./data/metadata/all_samples.csv','GSC','pH'), 4.63)
-        self.assertEqual(s1._get_experiment_property('./data/metadata/all_samples.csv','GSC','GWC %'), 24.7396979)
+        self.assertEqual(s1._get_experiment_property('./data/metadata/all_samples.csv','AA1_D1', 'GSC','pH'), 4.63)
+        self.assertEqual(s1._get_experiment_property('./data/metadata/all_samples.csv','AA1_D1','GSC','GWC %'), 24.7396979)
 
         with self.assertRaises(KeyError) as context:
-            s1._get_experiment_property('./data/metadata/all_samples.csv','GSC','NonExistingProperty')
+            s1._get_experiment_property('./data/metadata/all_samples.csv','AA1_D1','GSC','NonExistingProperty')
         self.assertTrue('Property NonExistingProperty not found in metadata file at row AA1_D1_GSC!' in str(context.exception))
 
         with self.assertRaises(KeyError) as context:
-            s1._get_experiment_property('./data/metadata/all_samples.csv','NonExistingExperiment','pH')
+            s1._get_experiment_property('./data/metadata/all_samples.csv','AA1_D1','NonExistingExperiment','pH')
         self.assertTrue('Sample AA1_D1_NonExistingExperiment not found in metadata file!' in str(context.exception))
+
+class TestDNAextractionSIPSample(unittest.TestCase):
+    def testPost(self):
+        s1 = DNAextractionSIPSample('A', 'A', '1', (0, 10), 'D1', '2020-01-01', './data/metadata/all_samples.csv', '16O', '7')
+
